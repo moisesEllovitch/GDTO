0f66cafd289afcafd084b5848123becb9daa5f85
Moises Ellovitch <moisesello@gmail.com>
2016-06-28 10:03:11 +0300

    Log out only

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,11 +18,9 @@ class SessionsController < ApplicationController
 		end
 	end
 
-	def destroy 
-	  session[:user_id] = nil 
-	  redirect_to '/' 
+	def destroy
+		log_out
+		redirect_to root_url
 	end
-
-
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,4 +9,10 @@ module SessionsHelper
 		@current_user ||= User.find_by(id: session[:user_id])
 	end
 
+	# Logs out the current user.
+	def log_out
+		session.delete(:user_id)
+		@current_user = nil
+	end
+
 end
