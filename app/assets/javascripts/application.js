edd7afb4535944454b1d5d32b4466d27a6b4bc7c
Moises Ellovitch <moisesello@gmail.com>
2017-02-01 15:01:40 +0200

    include Javascript: Validation of Sign Up form, and some design effect at the forms.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,66 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+/* Why to use Timing Event here?
+
+Because onload had a problem. The page still use application (same page) even if I changed page.
+So I would have to refresh every time.
+
+*/
+window.setInterval(main, 1000)
+
+function main(){
+
+	if (document.getElementById("form_user")){
+		formFeatured("form_user")
+		validateForm("form_user")
+	} 
+
+	if (document.getElementById("login_form")){
+		formFeatured("login_form")
+	}
+
+}
+
+
+function validateForm(form_id){
+	var form = document.getElementById(form_id)
+	//console.log(form)
+
+	form.onsubmit = function(){
+		var re_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		var re_pass = /^[A-Za-z]\w{4,}$/
+
+		if (!re_email.test(email.value)) {
+			console.log("Invalid email");
+			return false
+		} else if (!re_pass.test(pwd.value)) {
+			console.log("Invalid Password")
+			return false
+		} else	{
+			console.log("Validation successfull")
+			return true
+		}	
+	}
+}
+
+// set a style on a Sign Up and Login form when mouse over.
+function formFeatured(form_id){
+	var inputs = document.getElementsByClassName("js-style-featured")
+
+	document.getElementById(form_id).onmouseover = function(){
+		for (var i = 0; i < inputs.length; i++) {
+			//console.log('obj.' + i, '=', inputs[i]);
+			inputs[i].style.boxShadow = "0px 0px 30px #272626"
+		}
+	}
+
+	document.getElementById(form_id).onmouseout = function(){
+		for (var i = 0; i < inputs.length; i++) {
+			//console.log('obj.' + i, '=', inputs[i]);
+			inputs[i].style.boxShadow = ""
+		}
+	}
+}
